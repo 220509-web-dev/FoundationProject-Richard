6f7a909d37f4a186694abef4822f9344c6bc152a
Richard Moch <ibmpcdos5@gmail.com>
2022-05-23 22:17:43 -0500

    begin user model

---
+++ foundations/User.java
@@ -0,0 +1,66 @@
+public class User {
+    private int id;
+    private int role;
+    private String username;
+    private String password;
+
+    private String lastName;
+    private String firstName;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
