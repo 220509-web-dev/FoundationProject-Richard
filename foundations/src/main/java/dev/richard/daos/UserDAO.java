376dfb0925c9a304c1302759d0d311b7a097ee9a
Richard Moch <ibmpcdos5@gmail.com>
2022-05-26 15:16:56 -0500

    create userdao interface

--- foundations/src/main/java/dev/richard/daos/UserDAO.java
+++ foundations/src/main/java/dev/richard/daos/UserDAO.java
@@ -1,2 +1,16 @@
-package dev.richard.dao;public interface UserDAO {
+package dev.richard.daos;
+import dev.richard.entities.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    // Create
+    User createUser(User user);
+    // Read
+    User getUserById(User user);
+    List<User> getAllUsers();
+    // Update
+    User updateUser(User user);
+    // Delete
+    User deleteUser(User user);
 }
