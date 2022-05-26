327204dd9f27c816f018800e86dda7fba64dba4f
Richard Moch <ibmpcdos5@gmail.com>
2022-05-26 15:14:38 -0500

    create models

---
+++ foundations/src/main/java/dev/richard/daos/UserDAO.java
@@ -0,0 +1,2 @@
+package dev.richard.dao;public interface UserDAO {
+}
--- foundations/src/main/java/dev/richard/entities/Note.java
+++ foundations/src/main/java/dev/richard/entities/Note.java
@@ -1,5 +1,60 @@
 package dev.richard.entities;
 
 public class Note {
-    
+    private int noteId;
+    private String noteTitle;
+    private String noteBody;
+    private int ownerId;
+    private boolean visibility;
+
+    public Note() {
+
+    }
+
+    public Note(int noteId, String noteTitle, String noteBody, int ownerId, boolean visibility) {
+        this.noteId = noteId;
+        this.noteTitle = noteTitle;
+        this.noteBody = noteBody;
+        this.ownerId = ownerId;
+        this.visibility = visibility;
+    }
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteBody() {
+        return noteBody;
+    }
+
+    public void setNoteBody(String noteBody) {
+        this.noteBody = noteBody;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public boolean isVisible() {
+        return visibility;
+    }
+
+    public void setVisibility(boolean visibility) {
+        this.visibility = visibility;
+    }
 }
