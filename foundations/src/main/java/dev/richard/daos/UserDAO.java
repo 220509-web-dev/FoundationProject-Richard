c2dfd3f6b5657586a25a5a47ac404c9bfd914af6
Richard Moch <ibmpcdos5@gmail.com>
2022-05-26 21:38:48 -0500

    added some documentation to the userdao

--- foundations/src/main/java/dev/richard/daos/UserDAO.java
+++ foundations/src/main/java/dev/richard/daos/UserDAO.java
@@ -4,13 +4,42 @@ import dev.richard.entities.User;
 import java.util.List;
 
 public interface UserDAO {
+    /**
+     * Puts a new user into the database.
+     * @param user The user object to add.
+     * @return dev.richard.entities.User
+     */
     // Create
     User createUser(User user);
     // Read
+
+    /**
+     * Grabs a user in the database by their user ID.
+     * @param id The user ID to use.
+     * @return dev.richard.entities.User
+     */
+
     User getUserById(int id);
+
+    /**
+     * Gets all users in the database.
+     * @return java.util.List
+     */
     List<User> getAllUsers();
     // Update
+
+    /**
+     * Updates information about a user in the database.
+     * @param user The user object to pass.
+     * @return dev.richard.entities.User
+     */
     User updateUser(User user);
     // Delete
+
+    /**
+     * Deletes a user from the database.
+     * @param id The ID to delete.
+     * @return null if successful
+     */
     User deleteUserById(int id);
 }
--- foundations/src/main/java/dev/richard/daos/UserDaoPostgres.java
+++ foundations/src/main/java/dev/richard/daos/UserDaoPostgres.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoPostgres implements UserDAO {
-
     @Override
     public User createUser(User user) {
         try (Connection c = ConnectionUtil.getConnection()) {
