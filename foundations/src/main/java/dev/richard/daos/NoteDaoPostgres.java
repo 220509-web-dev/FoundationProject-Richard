dc0678798077235abef1e780f1140cecd7f88286
Richard Moch <ibmpcdos5@gmail.com>
2022-05-28 12:55:39 -0500

    begin implementation of notea dao

--- foundations/src/main/java/dev/richard/daos/NoteDaoPostgres.java
+++ foundations/src/main/java/dev/richard/daos/NoteDaoPostgres.java
@@ -1,5 +1,45 @@
 package dev.richard.daos;
 
-public class NoteDaoPostgres {
-// TODO: implement
+import dev.richard.entities.Note;
+import dev.richard.utils.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class NoteDaoPostgres implements NoteDAO {
+
+    @Override
+    public Note createNote(Note note) {
+        try (Connection c = ConnectionUtil.getConnection()) {
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Note getNoteById(int id) {
+        return null;
+    }
+
+    @Override
+    public List getNoteByAuthors(String username) {
+        return null;
+    }
+
+    @Override
+    public List getAllNotes() {
+        return null;
+    }
+
+    @Override
+    public Note updateNote(Note note) {
+        return null;
+    }
+
+    @Override
+    public Note deleteNoteById(Note note) {
+        return null;
+    }
 }
