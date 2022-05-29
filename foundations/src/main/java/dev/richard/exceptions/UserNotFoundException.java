8b76201ab31ab48281a6a7ee1abea1d0c632eff2
Richard Moch <ibmpcdos5@gmail.com>
2022-05-29 15:15:44 -0500

    add usernotfoundexception

--- foundations/src/main/java/dev/richard/exceptions/UserNotFoundException.java
+++ foundations/src/main/java/dev/richard/exceptions/UserNotFoundException.java
@@ -1,2 +1,10 @@
-package dev.richard.exceptions;public class UserNotFoundException {
+package dev.richard.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("The specified user does not exist.");
+    }
+    public UserNotFoundException(String message) {
+        super(String.format("The specified user does not exist. Additional information: %s", message));
+    }
 }
