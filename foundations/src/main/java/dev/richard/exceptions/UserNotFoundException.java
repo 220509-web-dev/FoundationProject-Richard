e444fd61193a6da668536012573bffa451beec83
Richard Moch <ibmpcdos5@gmail.com>
2022-05-31 14:17:13 -0500

    add custom exception

--- foundations/src/main/java/dev/richard/exceptions/UserNotFoundException.java
+++ foundations/src/main/java/dev/richard/exceptions/UserNotFoundException.java
@@ -7,4 +7,16 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(String.format("The specified user does not exist. Additional information: %s", message));
     }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
