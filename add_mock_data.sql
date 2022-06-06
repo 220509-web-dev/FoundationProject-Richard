96ef2298ba333a4075eeeec93f1a89b1d1fad174
Richard Moch <ibmpcdos5@gmail.com>
2022-06-06 13:12:52 -0500

    barebones mock data

--- add_mock_data.sql
+++ add_mock_data.sql
@@ -1,9 +1,3 @@
-set search_path to foundation_app;
+set search_path to soulnotes;
 
 insert into user_access (name) values ('BASIC'), ('PREMIUM'), ('ADMIN');
-
-insert into users (fname, lname, email, uname, pword, role_id) values ('Zumi', 'Daxuya', 'zumid@gmail.com', 'zumid', 'catboy', 1);
-insert into users (fname, lname, email, uname, pword, role_id) values ('Richard', 'Moch', 'richard@richardmoch.xyz', 'soulcatcher', 'testing', 2);
-
-insert into notes (name, note_body, owner_id, visible) values ('my super secret', 'meow', 1, true);
-insert into notes (name, note_body, owner_id, visible) values ('super super secret', 'no one can see this :D', 2, false);
